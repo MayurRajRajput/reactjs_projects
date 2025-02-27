5434ca29dc31d8d198fd4bb744d1792d779b3c4e
MAYUR RAJ <133855647+MayurRajRajput@users.noreply.github.com>
2025-02-28 02:32:19 +0530

    Add files via upload

---
+++ Dice_Game/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import StartGame from "./components/StartGame"
+import GamePlay from "./components/GamePlay"
+
+function App() {
+ const [isGameStarted, setIsGameStarted] = useState(false)
+ const toggle=()=>{
+  setIsGameStarted(prev=>!prev)
+ }
+  return (
+    <>
+    {
+      isGameStarted?<GamePlay/>:<StartGame toggle={toggle}/>
+    }
+    </>
+  )
+}
+
+export default App
